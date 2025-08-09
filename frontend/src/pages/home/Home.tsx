d2b9f7a5aeabfbc794ebb4ba2d49709e45db27aa
ananyosen <ananyo.sen2@gmail.com>
2025-08-09 11:07:36 +0530

    app bar on home page

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import { alpha, useTheme } from '@mui/material/styles';
 
 const Home: React.FC = () => {
-  return <h1>Home Page</h1>;
+  const theme = useTheme();
+  console.log(theme.palette)
+
+  useEffect(() => {
+    fetch('/api/assets')
+    .then(response => response.json())
+    .then(console.log);
+  }, []);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{
+        backdropFilter: 'blur(10px)',
+        backgroundColor: alpha(theme.palette.background.paper, 0.6),
+      }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <img src="/parchi.svg" alt="Parchi Logo" style={{ height: '32px' }} />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box component="main" sx={{ p: 3 }}>
+      </Box>
+    </Box>
+  )
 };
 
 export default Home;
